b2f0ad52433ab25112721db1066278b3487b29e9
mohamedfazilk <2114921@cognizant.com>
2022-07-21 13:36:51 +0530

    Adding the state values

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import List from './List';
+import Alert from './Alert';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
+  const [name, setName] = useState('')
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState (false)
+  const [editId, setEditId] = useState(null);
+  const [alert, setAlert] = useState({show: false, msg:'', type:'' }); // alert is an object showing these things
+
+  return <section>ggh</section>
 }
 
 export default App;
